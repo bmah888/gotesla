5f1dea666c4630381c5aa3856b7d323cd3eb28fe
Bruce A. Mah <bmah@es.net>
2019-03-02 10:07:59 -0800

    enh: Start of option decoding.

--- cmd/carinfo/main.go
+++ cmd/carinfo/main.go
@@ -19,6 +19,82 @@ import (
 	_ "time"
 )
 
+type OptionDecode struct {
+	OptionCode, Decode, OptionClass string
+}
+var decoder = [...]OptionDecode {
+	{OptionCode: "MDLS", Decode: "Model S", OptionClass: "model"},
+	{OptionCode: "MS03", Decode: "Model S", OptionClass: "model"},
+	{OptionCode: "MS04", Decode: "Model S", OptionClass: "model"},
+	{OptionCode: "MDLX", Decode: "Model X", OptionClass: "model"},
+	{OptionCode: "MDL3", Decode: "Model 3", OptionClass: "model"},
+	
+	{OptionCode: "APH0", Decode: "Autopilot 2.0 Hardware", OptionClass: "autopilothw"},
+	{OptionCode: "APH2", Decode: "Autopilot 2.0 Hardware", OptionClass: "autopilothw"},
+	{OptionCode: "APH3", Decode: "Autopilot 2.5 Hardware", OptionClass: "autopilothw"},
+	{OptionCode: "APPA", Decode: "Autopilot 1.0 Hardware", OptionClass: "autopilothw"},
+	{OptionCode: "APPB", Decode: "Enhanced Autopilot", OptionClass: "autopilothw"},
+
+	{OptionCode: "BP00", Decode: "No Ludicrous", OptionClass: "ludicrous"},
+	{OptionCode: "BP01", Decode: "Ludicrous Speed Upgrade", OptionClass: "ludicrous"},
+
+	{OptionCode: "BR00", Decode: "No battery firmware limit", OptionClass: "batterylimit"},
+	{OptionCode: "BR03", Decode: "Battery firmware limit (60 kWh)", OptionClass: "batterylimit"},
+	{OptionCode: "BR05", Decode: "Battery firmware limit (60 kWh)", OptionClass: "batterylimit"},
+
+	{OptionCode: "BT37", Decode: "75 kWh", OptionClass: "battery"},
+	{OptionCode: "BT40", Decode: "40 kWh", OptionClass: "battery"},
+	{OptionCode: "BT60", Decode: "60 kWh", OptionClass: "battery"},
+	{OptionCode: "BT70", Decode: "70 kWh", OptionClass: "battery"},
+	{OptionCode: "BT85", Decode: "85 kWh", OptionClass: "battery"},
+	{OptionCode: "BTX4", Decode: "90 kWh", OptionClass: "battery"},
+	{OptionCode: "BTX5", Decode: "75 kWh", OptionClass: "battery"},
+	{OptionCode: "BTX6", Decode: "100 kWh", OptionClass: "battery"},
+	{OptionCode: "BTX7", Decode: "75 kWh", OptionClass: "battery"},
+	{OptionCode: "BTX8", Decode: "85 kWh", OptionClass: "battery"},
+
+	{OptionCode: "CW00", Decode: "No Cold Weather Package", OptionClass: "subzero"},
+	{OptionCode: "CW01", Decode: "Cold Weather Package", OptionClass: "subzero"},
+
+	{OptionCode: "DA00", Decode: "No Autopilot", OptionClass: "autopilot"},
+	{OptionCode: "DA01", Decode: "Autopilot Active Safety", OptionClass: "autopilot"},
+	{OptionCode: "DA02", Decode: "Autopilot Convenience", OptionClass: "autopilot"},
+	{OptionCode: "DCF0", Decode: "Autopilot Convenience", OptionClass: "autopilot"},
+
+	{OptionCode: "DRLH", Decode: "Left-Hand Drive", OptionClass: "handedness"},
+	{OptionCode: "DRRH", Decode: "Right-Hand Drive", OptionClass: "handedness"},
+
+	{OptionCode: "DV2W", Decode: "RWD", OptionClass: "drivewheels"},
+	{OptionCode: "DV4W", Decode: "AWD", OptionClass: "drivewheels"},
+
+	{OptionCode: "TP01", Decode: "Tech Package (no AP)", OptionClass: "tech"},
+	{OptionCode: "TP02", Decode: "Tech Package (AP)", OptionClass: "tech"},
+	{OptionCode: "TP03", Decode: "Tech Package (EAP)", OptionClass: "tech"},
+}
+
+func printOptionCodes(codeString string) {
+	codeArray := strings.Split(codeString, ",")
+
+/*	decodes := make (map[string]OptionDecode) */
+
+	type optionDecode struct {
+		Decode, OptionCode string
+	}
+
+	for _, code := range codeArray {
+
+		for _, od := range decoder {
+			if od.OptionCode == code {
+				/*				decodes[od.OptionClass] = od */
+				fmt.Printf("%s %s\n", od.OptionCode, od.Decode)
+			}
+		}
+	}
+
+/*	fmt.Printf("%+v\n", decodes) */
+	
+}
+
 func main() {
 
 	// Command-line arguments
@@ -100,61 +176,76 @@ func main() {
 		fmt.Printf("Found id %d\n", idFound)
 	}
 
+/*
 	chs, err := gotesla.GetChargeState(client, token, idFound)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("GetChargeState: %s\n", err)
 		return
 	}
 	fmt.Printf("charge_state: %+v\n", chs)
 
 	cls, err := gotesla.GetClimateState(client, token, idFound)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("GetClimateState: %s\n", err)
 		return
 	}
 	fmt.Printf("climate_state: %+v\n", cls)
 
 	ds, err := gotesla.GetDriveState(client, token, idFound)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("GetDriveState: %s\n", err)
 		return
 	}
 	fmt.Printf("drive_state: %+v\n", ds)
 
 	gs, err := gotesla.GetGuiSettings(client, token, idFound)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("GetGuiSettings: %s\n", err)
 		return
 	}
 	fmt.Printf("gui_settings: %+v\n", gs)
 
 	vs, err := gotesla.GetVehicleState(client, token, idFound)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("GetVehicleState: %s\n", err)
 		return
 	}
 	fmt.Printf("vehicle_state: %+v\n", vs)
 
 	vc, err := gotesla.GetVehicleConfig(client, token, idFound)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("GetVehicleConfig: %s\n", err)
 		return
 	}
 	fmt.Printf("vehicle_config: %+v\n", vc)
-
+*/
 	mobileEnabled, err := gotesla.GetMobileEnabled(client, token, idFound)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("GetMobileEnabled: %s\n", err)
 		return
 	}
-	fmt.Printf("mobile_enabled: %+v\n", mobileEnabled)
+	if *verbose {
+		fmt.Printf("mobile_enabled: %+v\n", mobileEnabled)
+	}
 
 	vehicleData, err := gotesla.GetVehicleData(client, token, idFound)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("GetVehicleData: %s\n", err)
 		return
 	}
-	fmt.Printf("vehicle_data: %+v\n", vehicleData)
+	if *verbose {
+		fmt.Printf("vehicle_data: %+v\n", vehicleData)
+	}
+
+	// Print car ID stuff
+	fmt.Printf("VIN: %s\n", vehicleData.Vehicle.Vin)
+	fmt.Printf("ID: %d\n", vehicleData.Vehicle.Id)
+	fmt.Printf("VehicleID: %d\n", vehicleData.Vehicle.VehicleId)
+	fmt.Printf("DisplayName: %s\n", vehicleData.Vehicle.DisplayName)
+
+	// Decode options
+	fmt.Printf("OptionCodes: %s\n", vehicleData.Vehicle.OptionCodes)
+	printOptionCodes(vehicleData.Vehicle.OptionCodes)
 
 	return
 }
