23acf5181d74116953bb188336b328d9d15502b8
Bruce A. Mah <bmah@kitchenlab.org>
2019-11-27 23:01:50 -0800

    fix: Use better environment variable names.

--- cmd/scimport/docker-entrypoint.sh
+++ cmd/scimport/docker-entrypoint.sh
@@ -3,24 +3,24 @@ set -e
 
 ARGS=""
 
-if [ "x$PWI_INFLUX_DB" != 'x' ]; then
-	ARGS="$ARGS -influx-database $PWI_INFLUX_DB"
+if [ "x$SCI_INFLUX_DB" != 'x' ]; then
+	ARGS="$ARGS -influx-database $SCI_INFLUX_DB"
 fi
 
-if [ "x$PWI_INFLUX_MEASUREMENT" != 'x' ]; then
-	ARGS="$ARGS -influx-measurement $PWI_INFLUX_MEASUREMENT"
+if [ "x$SCI_INFLUX_MEASUREMENT" != 'x' ]; then
+	ARGS="$ARGS -influx-measurement $SCI_INFLUX_MEASUREMENT"
 fi
 
-if [ "x$PWI_INFLUX_URL" != 'x' ]; then
-	ARGS="$ARGS -influx-url $PWI_INFLUX_URL"
+if [ "x$SCI_INFLUX_URL" != 'x' ]; then
+	ARGS="$ARGS -influx-url $SCI_INFLUX_URL"
 fi
 
-if [ "x$PWI_POLL" != 'x' ]; then
-	ARGS="$ARGS -poll $PWI_POLL"
+if [ "x$SCI_POLL" != 'x' ]; then
+	ARGS="$ARGS -poll $SCI_POLL"
 fi
 
-if [ "x$PWI_TOKEN_CACHE" != 'x' ]; then
-	ARGS="$ARGS -token-cache $PWI_TOKEN_CACHE"
+if [ "x$SCI_TOKEN_CACHE" != 'x' ]; then
+	ARGS="$ARGS -token-cache $SCI_TOKEN_CACHE"
 fi
 
 /scimport $ARGS
